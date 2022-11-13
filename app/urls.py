6a8a6b68444c6816daa7c787e4063f81ae236205
jayanth756 <jayanthsaiadepu788@gmail.com>
2022-11-13 12:44:33 +0530

    5 commit

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminapp/', admin.site.urls),
     path('', include("login.urls")),
-    path('student/', include("student.urls")),
-    path('teacher/', include("teacher.urls")),
+    path('student/', include("customer.urls")),
+    path('teacher/', include("adminapp.urls")),
 ]
